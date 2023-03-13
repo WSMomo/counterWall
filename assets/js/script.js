38c7e96e6655c4d0aa368f7a6db710b734b26d3f
Momo M <momomarcoj@gmail.com>
2023-03-13 01:00:56 +0100

    cursore overflow funziona correttamente

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,32 @@ let arrayPos = [];
 let arrayNeg = [];
 let counterNumber = 0;
 
-
 //costruzione DOM
 const mainTag = document.querySelector('main');
 // creazione elementi e assegnazione
 const newCounter = newElement(mainTag, 'div', 'counter', String(counterNumber));
 const counter = document.querySelector('.counter');
-//parte sinistra/top
+
+//container
 const newWall = newElement(mainTag, 'div', 'wall', '');
 const wall = document.querySelector('.wall');
-
-//parte destra/bottom
 const newButtonsContainer = newElement(mainTag, 'div', 'buttons-container', '');
 const buttonsContainer = document.querySelector('.buttons-container');
+
+//parte bottom
 const newDecreaseButton = newElement(buttonsContainer, 'button', 'decrease-btn', '-');
 const newResetButton = newElement(buttonsContainer, 'button', 'reset-btn', 'RESET')
 const newIncreaseButton = newElement(buttonsContainer, 'button', 'increase-btn', '+');
 const decreaseBtn = document.querySelector('.decrease-btn');
 const resetBtn = document.querySelector('.reset-btn')
 const increaseBtn = document.querySelector('.increase-btn');
+
 increaseBtn.setAttribute('class', 'buttons');
 resetBtn.setAttribute('class', 'buttons')
 decreaseBtn.setAttribute('class', 'buttons');
-
 const buttons = document.querySelectorAll('.buttons');
+
+buttonsHover();
 
 //fa il render ad ogni aggiunta
 function renderWall(arr) {
@@ -34,25 +36,26 @@ function renderWall(arr) {
     arr.map((a) => {
         a = newElement(wall, 'div', 'newBrick', '');
     })
-    checkOverflow();
 }
 
-//DA SISTEMARE
+
 //controllo posizione cursore overflow
-function checkOverflow() {
-    if (arrayPos.length > 100) {
-        wall.scrollTop = wall.scrollHeight - wall.clientHeight;
-      } else if (arrayNeg.length > 100) {
-        wall.scrollTop = wall.scrollHeight - wall.clientHeight;
-}
-}
+ function checkOverflow() {
+    const bricks = document.querySelectorAll('.newBrick');
+     if (counterNumber > 100) {
+         wall.scrollTop = wall.scrollHeight - wall.clientHeight;
+     } else if (counterNumber < -100) {
+         wall.scrollBy(0, -bricks[0].offsetHeight);
+     }
+ }
+
 //Click dei buttons
 increaseBtn.addEventListener('click', () => {
     counterNumber++;
     counter.innerText = counterNumber;
     addOrRemovePos();
     changeBrickColor();
-    buttonsHover();
+    checkOverflow();
 })
 
 decreaseBtn.addEventListener('click', () => {
@@ -60,43 +63,41 @@ decreaseBtn.addEventListener('click', () => {
     counter.innerText = counterNumber;
     addOrRemoveNeg();
     changeBrickColor();
-    buttonsHover();
+    checkOverflow();
 })
 
-resetBtn.addEventListener('click', ()=>{
-    if(counterNumber>0){
+resetBtn.addEventListener('click', () => {
+    if (counterNumber > 0) {
         arrayPos = [];
-    } else{
+    } else {
         arrayNeg = [];
     }
     counterNumber = 0;
-        counter.innerText = counterNumber;
+    counter.innerText = counterNumber;
     wall.innerHTML = '';
-    buttonsHover();
-
 })
 
 
 //funzione per aggiungere l'hover e toglierla dopo il click (per evitare hover permanente su mobile)
-function buttonsHover(){
-    if('ontouchstart' in document.documentElement){
-        buttons.forEach((button)=>{
-            button.addEventListener('touchstart', () =>{
+function buttonsHover() {
+    if ('ontouchstart' in document.documentElement) {
+        buttons.forEach((button) => {
+            button.addEventListener('touchstart', () => {
                 button.classList.add('hover');
-            }, {passive: true});
-            button.addEventListener('touchend', ()=>{
+            }, { passive: true });
+            button.addEventListener('touchend', () => {
                 button.classList.remove('hover');
-        }, {passive: true});
-        button.addEventListener('click', ()=>{
-            button.classList.remove('hover');
-        }, {passive:true}); 
+            }, { passive: true });
+            button.addEventListener('click', () => {
+                button.classList.remove('hover');
+            }, { passive: true });
         });
     } else {
-        buttons.forEach((button)=>{
-            button.addEventListener('mouseenter', ()=>{
+        buttons.forEach((button) => {
+            button.addEventListener('mouseenter', () => {
                 button.classList.add('hover');
             });
-            button.addEventListener('mouseleave', ()=>{
+            button.addEventListener('mouseleave', () => {
                 button.classList.remove('hover');
             });
         });
